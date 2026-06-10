60e0a4f22a8b9b24ea162fef89949be1bb0d64b7
Mathin26 <mathinshack26@gmail.com>
2026-06-10 10:20:49 +0530

    Project update

--- gemma_app/lib/services/storage_service.dart
+++ gemma_app/lib/services/storage_service.dart
@@ -71,4 +71,15 @@ class StorageService {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(_speechLocaleKey);
   }
+  static const String _themeModeKey = 'theme_mode';
+
+Future<void> saveThemeMode(String mode) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString(_themeModeKey, mode);
+}
+
+Future<String> loadThemeMode() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString(_themeModeKey) ?? 'light';
+}
 }
